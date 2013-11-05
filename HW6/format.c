5dc1ff63f580f5e1fcaa873609cd849de1b8f355
Drew Gotbaum <dgotbaum@oberlin.edu>
2013-11-04 23:23:08 -0500

    Try this Prakash

--- HW6/format.c
+++ HW6/format.c
@@ -13,28 +13,7 @@
 int width = 72;
 char align = 'l';
 int spacing = 0;
-
-int main(int argc, const char * argv[])
-{
-    for (int i = 1; i < argc ; i++){
-        if (argv[i][0] == '-' && argv[i][1] == 'w') {
-            i++;
-            width = atoi(argv[i]);
-            printf("width changed to %d \n", width);
-        }
-        else if (argv[i][0] == '-' && (argv[i][1] == 'r'|| argv[i][1] == 'j')) {
-            align = argv[i][1];
-            printf("alignment is now %c \n",align );
-        }
-        else if (argv[i][0] == '-' && argv[i][1] == 's') {
-            spacing = 1;
-            printf("Spacing is on \n");
-        }
-        else if (argv[i][0] == '-') {
-            printf("Please use only the options from the man page.\n");
-            exit(1);
-        }
-    }
+void widthFormatting() {
     char ch;
     int count = 0;
     int lastspace = 0;
@@ -73,8 +52,31 @@ int main(int argc, const char * argv[])
             count++;
             wasLine = 0;
         }
-        
     }
+}
+int main(int argc, const char * argv[]){
+    for (int i = 1; i < argc ; i++){
+        if (argv[i][0] == '-' && argv[i][1] == 'w') {
+            i++;
+            width = atoi(argv[i]);
+            printf("width changed to %d \n", width);
+        }
+        else if (argv[i][0] == '-' && (argv[i][1] == 'r'|| argv[i][1] == 'j')) {
+            align = argv[i][1];
+            printf("alignment is now %c \n",align );
+        }
+        else if (argv[i][0] == '-' && argv[i][1] == 's') {
+            spacing = 1;
+            printf("Spacing is on \n");
+        }
+        else if (argv[i][0] == '-') {
+            printf("Please use only the options from the man page.\n");
+            exit(1);
+        }
+    }
+    
+
+    widthFormatting();
     printf("\n");
     return 0;
 }
