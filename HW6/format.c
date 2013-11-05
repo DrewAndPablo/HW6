c57ec8c68fac92e3a9cb978d88bb558c872a0ef3
Drew Gotbaum <dgotbaum@oberlin.edu>
2013-11-04 21:11:23 -0500

    finished main function
    it lets you know what options have been used as well

--- HW6/format.c
+++ HW6/format.c
@@ -20,15 +20,18 @@ int main(int argc, const char * argv[])
         if (argv[i][0] == '-' && argv[i][1] == 'w') {
             i++;
             width = atoi(argv[i]);
+            printf("width changed to %d \n", width);
         }
-        else if (argv[i][0] == '-' && (argv[i][1] == 'w'|| argv[i][1] == 'j')) {
+        else if (argv[i][0] == '-' && (argv[i][1] == 'r'|| argv[i][1] == 'j')) {
             align = argv[i][1];
+            printf("alignment is now %c \n",align );
         }
         else if (argv[i][0] == '-' && argv[i][1] == 's') {
             spacing = 1;
+            printf("Spacing is on \n");
         }
         else if (argv[i][0] == '-') {
-            printf("Please use only the options from the man page.");
+            printf("Please use only the options from the man page.\n");
             exit(1);
         }
     }
