cac50b4b83d7ab45bb6bd7db02824e0e6e6e87e7
Drew Gotbaum <dgotbaum@oberlin.edu>
2013-11-04 19:03:39 -0500

    Initial Commit

---
+++ HW6/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  HW6
+//
+//  Created by Drew Gotbaum on 11/4/13.
+//  Copyright (c) 2013 Drew Gotbaum. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
